c3da78eac772b412a63994578f32e348ed31e307
Alexander Lekomtsev <69035428+JI411@users.noreply.github.com>
2022-01-18 12:21:54 +0700

    Update sweep.py

--- utils/loggers/wandb/sweep.py
+++ utils/loggers/wandb/sweep.py
@@ -24,7 +24,7 @@ def sweep():
     opt.batch_size = hyp_dict.get("batch_size")
     opt.save_dir = str(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok or opt.evolve))
     opt.epochs = hyp_dict.get("epochs")
-    opt.nosave = True
+    opt.nosave = False
     opt.save_period = 5
     opt.data = hyp_dict.get("data")
     opt.weights = str(opt.weights)
